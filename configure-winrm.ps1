bf1658fd467266bd5815ab2e0521db1fd386a690
DESKTOP-NUTUO9P\thoma <thoschulte@googlemail.com>
2021-10-15 15:40:57 +0200

    fix

--- configure-winrm.ps1
+++ configure-winrm.ps1
@@ -1,24 +1,3 @@
-# # Supress network location Prompt
-# New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" -Force
-#
-# # Set network to private
-# $ifaceinfo = Get-NetConnectionProfile
-# Set-NetConnectionProfile -InterfaceIndex $ifaceinfo.InterfaceIndex -NetworkCategory Private
-#
-# # Set up WinRM and configure some things
-# winrm quickconfig -q
-# winrm s "winrm/config" '@{MaxTimeoutms="1800000"}'
-# winrm s "winrm/config/winrs" '@{MaxMemoryPerShellMB="2048"}'
-# winrm s "winrm/config/service" '@{AllowUnencrypted="true"}'
-# winrm s "winrm/config/service/auth" '@{Basic="true"}'
-#
-# # Enable the WinRM Firewall rule, which will likely already be enabled due to the 'winrm quickconfig' command above
-# Enable-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)"
-#
-# sc.exe config winrm start= auto
-#
-# exit 0
-
 $NetworkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
 $Connections = $NetworkListManager.GetNetworkConnections()
 $Connections | ForEach-Object { $_.GetNetwork().SetCategory(1) }
